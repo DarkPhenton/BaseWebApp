0efaef9571dbd159825ccf5832443c3ad06bbc7c
DarkPhenton <codefenton@outlook.com>
2024-01-05 18:13:12 -0600

    corrected lat and lon

--- project/start.js
+++ project/start.js
@@ -1,9 +1,9 @@
-import express, { static } from 'express';
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(static(__dirname));
+app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/html');
