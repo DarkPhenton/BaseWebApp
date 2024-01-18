d17b2ff186f8d60a8735298ef4fb33ca5502f181
DarkPhenton <codefenton@outlook.com>
2024-01-18 16:23:43 -0600

    fb auth and realtime db

--- project/js/main.js
+++ project/js/main.js
@@ -1,6 +1,39 @@
 /* $(document).ready(function() {
   getWeather();
 }) */
+import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+function handleSignIn() {
+  
+
+  const provider = new GoogleAuthProvider();
+
+  
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+    console.log(user.email);
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
 
 function getWeather(searchQuery) {
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+/*"lat=39&lon=-78*/"&units=imperial&appid="+ apiKey;
--- project/start.js
+++ project/start.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var firebase = require('firebase');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
