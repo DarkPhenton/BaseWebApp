ae5bfcd66319489dc8c53f28944e10a030f28de9
DarkPhenton <codefenton@outlook.com>
2024-02-07 19:15:28 -0600

    fb auth and realtime db

--- project/js/main.js
+++ project/js/main.js
@@ -1,15 +1,26 @@
 function handleSignIn() {
-  var provider = new firebaseui.auth.GoogleAuthProvider();
+  
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
 
-  firebaseui.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.credential.accessToken;
-    console.log(user.mail);
-  }).catch(function(error) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // IdP data available in result.additionalUserInfo.profile.
+      // ...
+  }).catch((error) => {
+    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    // The email of the user's account used.
     var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
+    // ...
   });
 }
 
