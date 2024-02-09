b482bdb7e887c6efa6e3d8a33f2002fc25526b28
DarkPhenton <codefenton@outlook.com>
2024-02-08 19:44:21 -0600

    fb auth and realtime db

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,44 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import { getDatabase} from 'firebase/database';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getDatabase(firebaseApp)
+
+export function handleSignIn() {
+  
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+  const firebaseConfig = {
+    authDomain: "message-stream-aadd0.firebaseapp.com",
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://message-stream-aadd0-default-rtdb.firebaseio.com",
+    projectId: "message-stream-aadd0",
+    storageBucket: "message-stream-aadd0.appspot.com",
+    messagingSenderId: "568130129310",
+    appId: "1:568130129310:web:7eaf7009be51f675ce1a8f",
+    measurementId: "G-5BZ7SF51HV"
+  };
+
 // function handleSignIn() {
   
 //   var provider = new firebase.auth.GoogleAuthProvider();
